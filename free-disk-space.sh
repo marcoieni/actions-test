f7646c1b93d44043d4ca08c8c2cf4fd004a1865a
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2025-08-13 13:11:07 +0200

    simplify

--- free-disk-space.sh
+++ free-disk-space.sh
@@ -3,8 +3,4 @@ set -euo pipefail
 
 script_dir=$(dirname "$0")
 
-if [[ "${RUNNER_OS:-}" == "Windows" ]]; then
-    python3 "$script_dir/free-disk-space-windows-start.py"
-else
-    $script_dir/free-disk-space-linux.sh
-fi
+python3 "$script_dir/free-disk-space-windows-start.py"
